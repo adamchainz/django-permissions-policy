346d08800864f8ac25e4be946b505a6b37820961
Adam Johnson <me@adamj.eu>
2019-05-26 11:27:15 +0100

    Fix testing on Django 1.11 (#28)
    For some reason the django111 requirements files had Django 2.2 installed instead, so Django 1.11 was never actually tested! Luckily no changes are needed outside of the test suite.

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
 from testapp.views import index
 
-urlpatterns = [
-    path('', index, name='index'),
-]
+try:
+    from django.urls import path
+
+    urlpatterns = [
+        path('', index, name='index'),
+    ]
+except ImportError:  # Django < 2.0
+    from django.conf.urls import url
+
+    urlpatterns = [
+        url(r"^$", index, name="index"),
+    ]
